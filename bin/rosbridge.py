82f9fbe04cbbb0b2cc1b435c8c64aace87b22469
Matthew Donoughe <mdonoughe@gmail.com>
2011-06-03 14:58:34 -0400

    rewrite to replace terribleWSS with txWebSocket
    the old setup used 100% CPU at idle on my test machine
    the new setup uses 0-1% CPU under a very light load
    
    txWebSocket is included in bin/twisted/web, but twisted itself is not
    it's my branch of txWebSocket that fixes a bug with closing connections
    https://github.com/mdonoughe/txWebSocket/
    
    This seems to work okay. A javascript echo server works, at least.

--- bin/rosbridge.py
+++ bin/rosbridge.py
@@ -1,170 +1,25 @@
 #!/usr/bin/python
 from ROSProxy import ROSProxy
-from terribleWSS import Session, serveForever
+import rospy; rospy.init_node('rosjs')
 import sys, traceback
 import json
-from threading import Lock, Thread
-from time import time, sleep
+from threading import Thread
+from twisted.internet import reactor
+from twisted.web.static import File
+from twisted.web.websocket import WebSocketHandler, WebSocketSite
+from time import time
 from urllib2 import urlopen
 import re
 
-class ServiceCaller(Thread):
-	def __init__(self, ros, session, service, arguments, callback):
-		Thread.__init__(self)
-		self.daemon = True
-		self.ros = ros
-		self.session = session
-		self.service = service
-		self.arguments = arguments
-		self.callback = callback
-
-	def run(self):
-		def handleResponse(rsp):
-			call = {'callback':self.callback,'msg':self.ros.generalize(rsp)}
-			self.session.sock.send(encode(call))
-		self.ros.callService(self.service,self.arguments,handleResponse)
-
 def encode(obj):
-	return '\x00' + json.dumps(obj) + '\xff'
-
-def handleFrameHelper(frame, session, handleMsgFactory, sub, keyurl, ros):
-	now = time()
-	if (not 'rosjsExtStarted' in dir(session)):
-		session.rosjsExtStarted = 0
-		session.rosjsExtDuration = 0
-
-	try:
-		call = ''
-		try:
-			call = json.loads(frame)
-		except:
-			call = json.loads(frame[1:])
-		receiver = call["receiver"]
-		msg = call["msg"]
-
-
-		if 'type' in call.keys():
-			#print "Publish Topic!"
-			if (not keyurl or (now - session.rosjsExtStarted <= session.rosjsExtDuration)):
-				typ = call["type"]
-
-				if not receiver in session.data.keys():
-					session.data[receiver] = rospy.Publisher(receiver.encode('ascii','ignore'), ros.msgClassFromTypeString(typ))
-
-				session.data[receiver].publish(ros.specify(typ,msg))
-
-		else:
-			#print "Service Call!"
-			callback = call["callback"]
-			if (not keyurl or (now - session.rosjsExtStarted <= session.rosjsExtDuration) or receiver.find('/rosjs/authorize') == 0):
-				if receiver.find('/rosjs') == 0:
-					call = {'callback':callback}
-					if (receiver == "/rosjs/topics"):
-						call['msg'] = ros.topics
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/services"):
-						call['msg'] = ros.services
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/subscribe"):
-						topic = msg[0].encode('ascii','ignore')
-						delay = msg[1]
-						sub(topic, handleMsgFactory(topic))
-						if len(msg) > 2:
-							session.data[topic] = {'delay':delay,'lastEmission':0,'lastSeq':0,'encoding':msg[2],'width':msg[3],'height':msg[4],'quality':msg[5]}
-						else:
-							session.data[topic] = {'delay':delay,'lastEmission':0,'lastSeq':0}
-						call['msg'] = 'OK'
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/log"):
-						filename = msg[0].encode('ascii','ignore')
-						filename = ''.join(c for c in filename if c.isalnum()) + '.log'
-						obj = msg[1];
-
-						success = True
-						try:
-							log = open(filename, 'w')
-							log.write(obj)
-							log.close()
-						except:
-							success = False
-
-						call['msg'] = 'OK'
-						if (not success):
-							call['msg'] = 'ERROR'
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/authorize"):
-						key = msg[0].encode('ascii','ignore')
-						duration = 0
-						if (keyurl):
-							req = urlopen(keyurl + '?jsonp=invoke&key=' + key)
-							req = req.read()
-							duration = int(re.sub('[^0-9]','',req))
-						session.rosjsExtStarted = now
-						session.rosjsExtDuration = duration
-						print "authorizing %s for %s seconds" % (key,duration)
-						call['msg'] = 'OK'
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/typeStringFromTopic"):
-						topic = msg[0].encode('ascii','ignore')
-						call['msg'] = ros.typeStringFromTopic(topic)
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/typeStringFromService"):
-						service = msg[0].encode('ascii','ignore');
-						call['msg'] = ros.typeStringFromService(service)
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/msgClassFromTypeString"):
-						typStr = msg[0].encode('ascii','ignore');
-						call['msg'] = ros.generalize(ros.msgClassFromTypeString(typStr)())
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/reqClassFromTypeString"):
-						typStr = msg[0].encode('ascii','ignore');
-						call['msg'] = ros.generalize(ros.srvClassFromTypeString(typStr)._request_class())
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/rspClassFromTypeString"):
-						typStr = msg[0].encode('ascii','ignore');
-						call['msg'] = ros.generalize(ros.srvClassFromTypeString(typStr)._response_class())
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/classFromTopic"):
-						topic = msg[0].encode('ascii','ignore')
-						call['msg'] = ros.generalize(ros.classFromTopic(topic)())
-						session.sock.send(encode(call))
-					elif (receiver == "/rosjs/classesFromService"):
-						service = msg[0].encode('ascii','ignore')
-						call['msg'] = {'req':ros.generalize(ros.classFromService(service)._request_class()),'rsp':ros.generalize(ros.classFromService(service)._response_class())}
-						session.sock.send(encode(call))
-				else:
-					idx = receiver.find('protected')
-					if idx >= 0 and idx <=1:
-						print "ignoring call to protected service"
-						call = {'callback':callback,'msg':{}}
-						session.sock.send(encode(call))
-					else:
-						cls = ros.classFromService(receiver)
-						serviceCaller = ServiceCaller(ros,session,receiver,ros.specify(cls._request_class._slot_types,msg),callback)
-						serviceCaller.start()
-
-	except:
-		print "Problem " * 10
-		traceback.print_exc()
-		print "Problem " * 10
-
-	if (not keyurl or (now - session.rosjsExtStarted <= session.rosjsExtDuration)):
-		session.transition(Session.ready)
-	else:
-		session.transition(Session.closed)
-
-def handleFrameFactory(handleMsgFactory, sub, keyurl, ros):
-	def handleFrame(frame, session):
-		return handleFrameHelper(frame, session, handleMsgFactory, sub, keyurl, ros)
-	return handleFrame
+	return json.dumps(obj)
 
 if __name__ == "__main__":
 	ros = ROSProxy()
-	import rospy
 	rospy.init_node('rosjs')
 
 	latest = {}
-	latestLock = Lock()
+	openSockets = []
 
 	keyurl = rospy.get_param('/brown/rosjs/keyurl','')
 	if "--keyurl" in sys.argv:
@@ -196,84 +51,243 @@ if __name__ == "__main__":
 			print "Please provide a port number."
 			sys.exit(-1)
 
-	def sub(topic, handler):
-		idx = topic.find('protected')
-		if idx >= 0 and idx <=1:
-			print "ignoring request to listen to protected topic"
-			return
-		latestLock.acquire()
-		repeat = False
-		if topic in latest.keys():
-			repeat = True
-		latestLock.release()
+	class Handler(WebSocketHandler):
+		def connectionMade(self):
+			print('Connection from %s' % self.transport.getPeer())
+			self.publishers = {}
+			self.subscribers = {}
+			self.authed = not self.keyurl
+			self.openSockets.append(self)
 
-		if not repeat:
-			print "subscribing to: %s" % (topic,)
-			cls = ros.classFromTopic(topic)
-			rospy.Subscriber(topic, cls, handler, queue_size=1)
-
-	def handleMsgFactory(topic):
-		def handleMsg(msg):
-			latestLock.acquire()
-
-			seq = 0
+		def sub(self, topic, handler):
+			idx = topic.find('protected')
+			if idx >= 0 and idx <=1:
+				print "ignoring request to listen to protected topic"
+				return
+			repeat = False
 			if topic in latest.keys():
-				seq = latest[topic]['seq']
-			latest[topic] = {'seq':seq + 1,'msg':msg}
+				repeat = True
 
-			latestLock.release()
+			if not repeat:
+				print "subscribing to: %s" % (topic,)
+				cls = self.ros.classFromTopic(topic)
+				rospy.Subscriber(topic, cls, handler, queue_size=1)
 
-		return handleMsg
+		def frameReceived(self, frame):
+			print(frame)
+			call = json.loads(frame)
+			receiver = call["receiver"]
+			msg = call["msg"]
 
-	def handleOutput(session):
-		if (keyurl):
-			if ('rosjsExtStarted' in dir(session) and time() - session.rosjsExtStarted > session.rosjsExtDuration):
-				session.transition(Session.closed)
+			if 'type' in call.keys():
+				#print "Publish Topic!"
+				if self.authed:
+					typ = call["type"]
+
+					if not receiver in self.publishers:
+						self.publishers[receiver] = rospy.Publisher(receiver.encode('ascii','ignore'), ros.msgClassFromTypeString(typ))
+
+					self.publishers[receiver].publish(ros.specify(typ,msg))
+			else:
+				#print "Service Call!"
+				callback = call["callback"]
+				if self.authed or receiver.startswith('/rosjs/authorize'):
+					if receiver.startswith('/rosjs'):
+						call = {'callback':callback}
+						if (receiver == "/rosjs/topics"):
+							call['msg'] = ros.topics
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/services"):
+							call['msg'] = ros.services
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/subscribe"):
+							topic = msg[0].encode('ascii','ignore')
+							delay = msg[1]
+							self.sub(topic, handleMsgFactory(topic))
+							if len(msg) > 2:
+								self.subscribers[topic] = {'delay':delay,'lastEmission':0,'lastSeq':0,'encoding':msg[2],'width':msg[3],'height':msg[4],'quality':msg[5]}
+							else:
+								self.subscribers[topic] = {'delay':delay,'lastEmission':0,'lastSeq':0}
+							call['msg'] = 'OK'
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/log"):
+							filename = msg[0].encode('ascii','ignore')
+							filename = ''.join(c for c in filename if c.isalnum()) + '.log'
+							obj = msg[1];
+
+							success = True
+							try:
+								log = open(filename, 'w')
+								log.write(obj)
+								log.close()
+							except:
+								success = False
+
+							call['msg'] = 'OK'
+							if (not success):
+								call['msg'] = 'ERROR'
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/authorize"):
+							key = msg[0].encode('ascii','ignore')
+							if (self.keyurl):
+								reactor.callInThread(self.doAuth, self, key, callback)
+							else:
+								call['msg'] = 'OK'
+								self.transport.write(encode(call))
+						elif (receiver == "/rosjs/typeStringFromTopic"):
+							topic = msg[0].encode('ascii','ignore')
+							call['msg'] = ros.typeStringFromTopic(topic)
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/typeStringFromService"):
+							service = msg[0].encode('ascii','ignore');
+							call['msg'] = ros.typeStringFromService(service)
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/msgClassFromTypeString"):
+							typStr = msg[0].encode('ascii','ignore');
+							call['msg'] = ros.generalize(ros.msgClassFromTypeString(typStr)())
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/reqClassFromTypeString"):
+							typStr = msg[0].encode('ascii','ignore');
+							call['msg'] = ros.generalize(ros.srvClassFromTypeString(typStr)._request_class())
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/rspClassFromTypeString"):
+							typStr = msg[0].encode('ascii','ignore');
+							call['msg'] = ros.generalize(ros.srvClassFromTypeString(typStr)._response_class())
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/classFromTopic"):
+							topic = msg[0].encode('ascii','ignore')
+							call['msg'] = ros.generalize(ros.classFromTopic(topic)())
+							self.transport.write(encode(call))
+						elif (receiver == "/rosjs/classesFromService"):
+							service = msg[0].encode('ascii','ignore')
+							call['msg'] = {'req':ros.generalize(ros.classFromService(service)._request_class()),'rsp':ros.generalize(ros.classFromService(service)._response_class())}
+							self.transport.write(encode(call))
+					else:
+						idx = receiver.find('protected')
+						if idx >= 0 and idx <=1:
+							print "ignoring call to protected service"
+							# is this really the right thing to do?
+							call = {'callback':callback,'msg':{}}
+							self.transport.write(encode(call))
+						else:
+							cls = ros.classFromService(receiver)
+							def handleResponse(rsp):
+								def completeResponse():
+									call = {'callback':callback,'msg':self.ros.generalize(rsp)}
+									self.transport.write(encode(call))
+								reactor.callFromThread(completeResponse)
+							reactor.callInThread(self.ros.callService, self.ros, receiver, ros.specify(cls._request_class._slot_types,msg), handleResponse)
+
+			if not self.authed:
+				self.transport.loseConnection()
+
+		# runs in a background thread
+		def doAuth(self, key, callback):
+			duration = 0
+			try:
+				req = urlopen(self.keyurl + '?jsonp=invoke&key=' + key)
+				req = req.read()
+				duration = int(re.sub('[^0-9]','',req))
+			except e:
+				print('auth error: %s' % e)
+			reactor.callFromThread(self.finishAuth, self, duration)
+
+		def finishAuth(self, duration, callback):
+			if duration > 0:
+				self.authed = True
+				if self.deauth and self.deauth.active():
+					self.deauth.reset(duration)
+				else:
+					self.deauth = reactor.callLater(duration, self.doDeauth, self)
+				self.transport.write(encode({'callback':callback,'msg':'OK'}))
+			else:
+				self.transport.write(encode({'callback':callback,'msg':'ERROR'}))
+
+		def doDeauth(self):
+			print('deauth')
+			self.authed = False
+
+		def sendLatest(self, topic):
+			if not self.authed:
+				self.transport.loseConnection()
 				return
 
-		for topic in session.data.keys():
-			if hasattr(session.data[topic],'publish'):
-				continue
-			delay = session.data[topic]['delay']
-			lastEmission = session.data[topic]['lastEmission']
-			lastSeq = session.data[topic]['lastSeq']
-			data = session.data[topic]
+			if topic in self.subscribers and topic in latest:
+				delay = self.subscribers[topic]['delay']
+				lastEmission = self.subscribers[topic]['lastEmission']
+				lastSeq = self.subscribers[topic]['lastSeq']
+				data = self.subscribers[topic]
 
-			latestLock.acquire()
-
-			if topic in latest.keys():
 				current = latest[topic]
 
-				#release as soon as possible
-				latestLock.release()
+				if 'encoding' in data:
+					call = {'receiver':topic, 'msg': ros.generalize(current['msg'],data['encoding'],data['width'],data['height'],data['quality'])}
+				else:
+					call = {'receiver':topic, 'msg': ros.generalize(current['msg'])}
+				self.transport.write(encode(call))
+				data['lastEmission'] = time()
+				data['lastSeq'] = current['seq']
 
-				if current['seq'] > lastSeq:
-					elapsed = (time() - lastEmission)*1000
-					if elapsed > delay:
-						if 'encoding' in data:
-							call = {'receiver':topic, 'msg': ros.generalize(current['msg'],data['encoding'],data['width'],data['height'],data['quality'])}
+		def connectionLost(self, reason):
+			print('closed %s: %s' % (self.transport.getPeer(), reason))
+			# don't do this because it can break rosproxy
+			#for topic in self.publishers:
+				#publisher = self.publishers[topic]
+				#publisher.unregister()
+			self.publishers = {}
+			for topic in self.subscribers:
+				subscriber = self.subscribers[topic]
+				if 'futureSend' in subscriber and subscriber['futureSend'].active():
+					subscriber['futureSend'].cancel()
+			self.subscribers = {}
+			self.openSockets.remove(self)
+
+	def wsHandlerFactory(transport):
+		handler = Handler(transport)
+		handler.keyurl = keyurl
+		handler.ros = ros
+		handler.openSockets = openSockets
+		handler.latest = latest
+		return handler
+
+	def handleMsgFactory(topic):
+		# this is called by the ROS thread
+		def handleMsg(msg):
+			# call in the reactor thread
+			def handle(msg):
+				seq = 0
+				if topic in latest.keys():
+					seq = latest[topic]['seq']
+				latest[topic] = {'seq':seq + 1,'msg':msg}
+				for socket in openSockets:
+					if topic in socket.subscribers:
+						sub = socket.subscribers[topic]
+						elapsed = time() - sub['lastEmission']
+						if elapsed * 1000 > sub['delay']:
+							socket.sendLatest(topic)
 						else:
-							call = {'receiver':topic, 'msg': ros.generalize(current['msg'])}
-						session.sock.send(encode(call))
-						if 'encoding' in data: 
-							session.data[topic] = {'delay':delay,'lastEmission':time(),'lastSeq':current['seq'],'encoding':data['encoding'],'width':data['width'],'height':data['height'],'quality':data['quality']}
-						else:
-							session.data[topic] = {'delay':delay,'lastEmission':time(),'lastSeq':current['seq']}
-			else:
-				latestLock.release()
+							if not ('futureSend' in sub and sub['futureSend'].active()):
+								sub['futureSend'] = reactor.callLater(sub['delay'] / 1000.0 - elapsed, socket.sendLatest, topic)
+			reactor.callFromThread(handle, msg)
+		return handleMsg
 
-	def loop():
-		return not rospy.is_shutdown()
+	def run_ros_thread():
+		def wait_for_exit():
+			rospy.spin()
+			reactor.stop()
+		# don't use reactor.callInThread or we might clog the thread pool
+		thread = Thread(target = wait_for_exit)
+		# the ros thread won't stop on exit
+		thread.setDaemon(True)
+		thread.start()
+	reactor.callWhenRunning(run_ros_thread)
 
-	restart = True
-	while loop():
-		if restart:
-			restart = False
-			try:
-				serveForever(handleFrameFactory(handleMsgFactory, sub, keyurl, ros), handleOutput, loop, host, port)
-			except:
-				restart = True
-				print "Problem " * 10
-				traceback.print_exc()
-				print "Problem " * 10
-		sleep(1)
+	#TODO: this path should be read from a preference with a different default
+	root = File('.')
+	#TODO: this should probably support exporting the script on top of a user specified file system
+	site = WebSocketSite(root)
+	#TODO: this path should be read from a preference
+	site.addHandler('/ws', wsHandlerFactory)
+	# 50 is the default backlog size
+	reactor.listenTCP(port, site, 50, host)
+	reactor.run()
